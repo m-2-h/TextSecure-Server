bbba13d76673b7397a45a3b5136675a70714e093
Andrei Maiseyenka <andrei.maiseyenka@gmail.com>
2017-10-12 17:31:06 +0200

    Enabled silent push

--- src/main/java/org/whispersystems/textsecuregcm/push/PushSender.java
+++ src/main/java/org/whispersystems/textsecuregcm/push/PushSender.java
@@ -41,7 +41,7 @@ public class PushSender implements Managed {
 
   private final Logger logger = LoggerFactory.getLogger(PushSender.class);
 
-  private static final String APN_PAYLOAD = "{\"aps\":{\"sound\":\"default\",\"badge\":%d,\"alert\":{\"loc-key\":\"APN_Message\"}}}";
+  private static final String APN_PAYLOAD = "{\"aps\":{\"content-available\":1}}";
 
   private final ApnFallbackManager         apnFallbackManager;
   private final PushServiceClient          pushServiceClient;
@@ -117,20 +117,19 @@ public class PushSender implements Managed {
 
     if (!Util.isEmpty(device.getVoipApnId())) {
       apnMessage = new ApnMessage(device.getVoipApnId(), account.getNumber(), (int)device.getId(),
-                                  String.format(APN_PAYLOAD, messageQueueDepth),
+                                  APN_PAYLOAD,
                                   true, System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(30));
 
       apnFallbackManager.schedule(new WebsocketAddress(account.getNumber(), device.getId()),
                                   new ApnFallbackTask(device.getApnId(), apnMessage));
     } else {
       apnMessage = new ApnMessage(device.getApnId(), account.getNumber(), (int)device.getId(),
-                                  String.format(APN_PAYLOAD, messageQueueDepth),
+                                  APN_PAYLOAD,
                                   false, ApnMessage.MAX_EXPIRATION);
     }
 
     try {
       pushServiceClient.send(apnMessage);
-      logger.info(account.getNumber() + " " + (apnMessage.isVoip() ? "voip: " : "apn: ") + apnMessage.getApnId());
     } catch (TransientPushFailureException e) {
       logger.warn("SILENT PUSH LOSS", e);
     }
